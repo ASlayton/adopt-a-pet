d7cb8718ab1cae86ea0e8f821a048ef05467ff22
Amanda Slayton <alslayton86@gmail.com>
2018-04-13 22:08:48 -0500

    wip

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,7 +5,7 @@ const writeToDom = (myInnerds, myElement) => {
 const buildDomString = (petArray) => {
   let myString = '';
   petArray.forEach(pet => {
-    myString += `<div class="panel panel-default ${pet.type}">`;
+    myString += `<div class="panel panel-default ${pet.type} col-sm-4">`;
     myString +=   `<div class="panel-heading">`;
     myString +=     `<h3 class="panel-title">${pet.name}</h3>`;
     myString +=   `</div>`;
@@ -14,7 +14,13 @@ const buildDomString = (petArray) => {
     myString +=     `<p>${pet.color}</p>`;
     myString +=     `<p>${pet.specialSkill}</p>`;
     myString +=   `</div>`;
-    myString +=   `<div class="panel-footer">${pet.type}</div>`;
+    if (`${pet.type.toLowerCase()}` === 'dino') {
+      myString +=   `<div class="panel-footer dino-foot">${pet.type}</div>`;
+    } else if (`${pet.type.toLowerCase()}` === 'dog') {
+      myString +=   `<div class="panel-footer dog-foot">${pet.type}</div>`;
+    } else {
+      myString +=   `<div class="panel-footer cat-foot">${pet.type}</div>`;
+    };
     myString += `</div>`;
   });
   writeToDom(myString, 'animal-container');
